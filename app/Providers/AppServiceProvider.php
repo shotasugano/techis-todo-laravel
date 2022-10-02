75401d5d4d49f874af63909a10e80fc87ae8d744
shotasugano <10sugano18@gmail.com>
2022-10-02 14:23:37 +0900

    サービスプロバイダの修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if(\App::environment(['production'])){
-            \URL::forseScheme('https');
+            \URL::forceScheme('https');
         }
     }
 }
